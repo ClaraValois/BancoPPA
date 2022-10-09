3fb7183f594b4d3ec2dce369f5ddb6aafa80c44b
Clara Valois <107444889+ClaraValois@users.noreply.github.com>
2022-10-09 13:23:28 -0300

    Update bancoPPA.sql

--- bancoPPA.sql
+++ bancoPPA.sql
@@ -1,48 +1,47 @@
-create schema sistema_almoxarifado;
-use sistema_almoxarifado;
+create schema almox;
+use almox;
 
- create table Area_depar(
+ create table area_depar(
  id_area integer auto_increment primary key,
- nome_Dep varchar (40)
+ nome varchar (40)
  );
-CREATE TABLE Servidor ( 
- id_Servidor integer auto_increment primary key,
- Nome varchar(40),  
- tel integer,  
- tipo integer,  
+CREATE TABLE servidor ( 
+ id_servidor integer auto_increment primary key,
+ nome varchar(40),  
+ cpf varchar (15), 
+ telefone varchar (11),  
  email varchar (80),  
 id_area integer not null,
-foreign key (id_area)  references Area_depar (id_area)); 
-
-CREATE TABLE Requisicao( 
- status varchar(10),  
+foreign key (id_area)  references area_depar (id_area)); 
+CREATE TABLE requisicao( 
+ stats varchar(10),  
  id_req integer auto_increment primary key, 
  dataReq date,  
- id_Servidor integer not null,
- foreign key (id_Servidor) references Servidor (id_Servidor)
+ id_servidor integer not null,
+ foreign key (id_servidor) references servidor (id_servidor)
 ); 
 
 
-CREATE TABLE Modelo ( 
+CREATE TABLE modelo ( 
  id_modelo integer auto_increment primary key,  
- descricao varchar(30)); 
+ descricao_mod varchar(30)); 
 
-CREATE TABLE Marca( 
- descricao  integer,  
+CREATE TABLE marca( 
+ descricao_marca  integer,  
  id_marca integer auto_increment primary key); 
  
- CREATE TABLE ValorProduto ( 
+ CREATE TABLE valorProduto ( 
  id_valor integer auto_increment primary key,
  lote integer,  
- ValorLote float); 
+ valorLote float); 
 
-CREATE TABLE TipoProduto 
+CREATE TABLE tipoProduto 
 (  id_tipo integer auto_increment primary key,  
- descricao varchar (30)); 
+ descricao_tipo varchar (30)); 
 
-create table Produto( id_Prod INT PRIMARY KEY,  
+create table produto( id_prod INT PRIMARY KEY,  
  quantDisp integer,  
- Nome varchar(30),  
+ nome_prod varchar(30),  
  id_marca integer not null,  
  id_modelo integer not null,  
  id_valor integer not null,
@@ -53,15 +52,15 @@ foreign key  (id_marca ) references Marca(id_marca ),
  foreign key  (id_valor) references ValorProduto(id_valor),
   foreign key  ( id_tipo) references TipoProduto( id_tipo)
 ); 
-CREATE TABLE itemReq ( 
- quantSol integer not null,  
+CREATE TABLE itemreq ( 
+ quantsol integer not null,  
  id_req integer not null,  
- id_Prod integer not null, 
+ id_prod integer not null, 
  foreign key (id_req) references Requisicao (id_req),
- foreign key (id_Prod) references Produto( id_Prod)
+ foreign key (id_Prod) references Produto( id_prod)
 ); 
 
-CREATE TABLE NF 
+CREATE TABLE notafiscal
 ( 
  id_nf integer auto_increment primary key,
  num_serie integer,  
@@ -69,21 +68,23 @@ CREATE TABLE NF
  CNPJ integer  
 ); 
  
-CREATE TABLE Entrada ( 
+CREATE TABLE entrada ( 
  qtd integer,  
  id_entrada integer auto_increment primary key,
- id_Prod integer not null,  
+ id_prod integer not null,  
  id_nf integer not null,
- foreign key (id_nf) references NF(id_nf),
- foreign key (id_Prod) references Produto (id_Prod)
+ foreign key (id_nf) references notafiscal (id_nf),
+ foreign key (id_prod) references Produto (id_prod)
  ); 
  
- alter table Servidor add column id_chefia integer ;
- alter table Servidor add constraint foreign key (id_chefia) references Servidor (id_Servidor);
+ alter table servidor add column id_chefia integer ;
+ alter table servidor add constraint foreign key (id_chefia) references servidor (id_servidor);
 
-create table usuarios(
-    id_user integer auto_increment primary key,
+CREATE TABLE usuarios (
+    id_user INTEGER AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(140),
-    email VARCHAR(140) not null,
+    email VARCHAR(140) NOT NULL,
     senha VARCHAR(16)
 );
+
+select * from servidor;
